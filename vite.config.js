defe7726895905aac0377a048ff2f44963fe1915
turd <182042136+raymondfarts@users.noreply.github.com>
2025-01-08 19:44:19 -0500

    uibuild

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  host: true,
-  strictPort: true,
-  port: 8000,
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3001,
+    }
   // server: { proxy: { '/api': { target: 'http://localhost:3001', changeOrigin: true, }, } },
 })
